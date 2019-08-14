d8f551fa2f5d63f054dce3edc01c4037aadc807c
ahmady016 <ahmady09@gmail.com>
2019-08-14 11:32:53 +0200

    styling ColorBox

--- src/palettes/Palette.js
+++ src/palettes/Palette.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from '@material-ui/core/Button'
 
 const PaletteWrapper = styled.div`
   height: 100%;
@@ -9,18 +10,51 @@ const PaletteColors = styled.div`
   display: flex;
   flex-wrap: wrap;
 `
+const ColorBoxInnerWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  font-size: 0.8rem;
+`
+const ColorBoxContent = styled(ColorBoxInnerWrapper)`
+  justify-content: space-between;
+  align-items: flex-end;
+`
+const ColorName = styled.div`
+  text-transform: uppercase;
+`
+const ButtonMore = styled(Button)`
+  border-radius: 0 !important;
+`
+const ColorBoxCopyWrapper = styled(ColorBoxInnerWrapper)`
+  flex-basis: 40%;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+`
 const ColorBoxWrapper = styled.div`
+  position: relative;
   cursor: pointer;
   background-color: ${({ color }) => color};
-  height: 25%;
-  flex-basis: 20%;
+  flex-basis: 25%;
+  height: 40%;
+  &:hover ${ColorBoxCopyWrapper} {
+    opacity: 1;
+    transition: opacity 0.5s ease-out;
+  }
 `
 
 function ColorBox ({ name, color }) {
   return (
     <ColorBoxWrapper color={color}>
-      <span>{name}</span>
-      <span>MORE</span>
+      <ColorBoxContent>
+        <ColorName>{name}</ColorName>
+        <ButtonMore size='small' variant='contained'>MORE</ButtonMore>
+      </ColorBoxContent>
+      <ColorBoxCopyWrapper>
+        <Button variant='contained'>COPY</Button>
+      </ColorBoxCopyWrapper>
     </ColorBoxWrapper>
   )
 }
