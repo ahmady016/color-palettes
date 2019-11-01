76f74f008b844d0fd4760aa3c9c8dcdb38b222f9
ahmady016 <ahmady09@gmail.com>
2019-11-01 07:44:32 +0200

    Add Delete Confirm Dialog in PaletteList.js

--- src/palettes/PaletteList.js
+++ src/palettes/PaletteList.js
@@ -11,6 +11,18 @@ import Avatar from '@material-ui/core/Avatar'
 import Divider from '@material-ui/core/Divider'
 import DeleteIcon from '@material-ui/icons/Delete'
 
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemText from '@material-ui/core/ListItemText'
+import CheckIcon from '@material-ui/icons/Check'
+import CloseIcon from '@material-ui/icons/Close'
+
+import blue from '@material-ui/core/colors/blue'
+import red from '@material-ui/core/colors/red'
+
 //#region styled components
 const PaletteListWrapper = styled.div`
   background-color: #0057e7;
@@ -76,17 +88,54 @@ const PaletteName = styled.span`
   font-weight: 700;
   color: #444;
 `
+const ColoredAvatar = styled(Avatar)`
+  background-color: ${ ({ color }) => color[100] } !important;
+  color: ${ ({ color }) => color[600] } !important;
+`
 //#endregion
 
 const goToPalette = (id, history) => e => void history.push(`/palette/${id}`)
 
 const ColorBox = ({ color }) => <ColorBoxWrapper color={color} />
 
-function PaletteCard({ id, name, emoji, colors, history, setPalettes }) {
-  const deletePalette = e => setPalettes(palettes => palettes.filter(palette => palette.id !== id))
+function ConfirmDeleteDialog({ opened, setOpened, setPalettes, paletteIdToDelete }) {
+  const deletePalette = e => {
+    setPalettes(palettes => palettes.filter(palette => palette.id !== paletteIdToDelete))
+    setOpened(false)
+  }
+  return (
+    <Dialog open={opened} onClose={() => setOpened(false)} aria-labelledby="simple-dialog-title">
+      <DialogTitle>Are you sure deleting this Palette ?</DialogTitle>
+      <List>
+        <ListItem button onClick={deletePalette}>
+          <ListItemAvatar>
+            <ColoredAvatar color={blue}>
+              <CheckIcon />
+            </ColoredAvatar>
+          </ListItemAvatar>
+          <ListItemText primary="Delete" />
+        </ListItem>
+        <ListItem autoFocus button onClick={e => setOpened(false)}>
+          <ListItemAvatar>
+            <ColoredAvatar color={red}>
+              <CloseIcon />
+            </ColoredAvatar>
+          </ListItemAvatar>
+          <ListItemText primary="Cancel" />
+        </ListItem>
+      </List>
+    </Dialog>
+  )
+}
+
+function PaletteCard({ id, name, emoji, colors, history, setOpened, setPaletteIdToDelete }) {
+  const toDelete = id => e => {
+    setOpened(true)
+    setPaletteIdToDelete(id)
+  }
   return (
     <CardDiv>
-      <DeletePaletteDiv onClick={deletePalette}>
+      <DeletePaletteDiv onClick={toDelete(id)}>
         <DeleteIcon />
       </DeletePaletteDiv>
       <CardActionArea onClick={goToPalette(id, history)}>
@@ -106,6 +155,8 @@ function PaletteCard({ id, name, emoji, colors, history, setPalettes }) {
 }
 
 function PaletteList({ palettes, setPalettes, history }) {
+  const [opened, setOpened] = React.useState(false)
+  const [paletteIdToDelete, setPaletteIdToDelete] = React.useState(true)
   return (
     <PaletteListWrapper>
       <PaletteListTitle className='flex-between'>
@@ -121,9 +172,10 @@ function PaletteList({ palettes, setPalettes, history }) {
       </PaletteListTitle>
       <PaletteListItems>
         {palettes.map(palette =>
-          <PaletteCard key={palette.id} history={history} {...palette} setPalettes={setPalettes} />
+          <PaletteCard key={palette.id} history={history} {...palette} setOpened={setOpened} setPaletteIdToDelete={setPaletteIdToDelete} />
         )}
       </PaletteListItems>
+      <ConfirmDeleteDialog opened={opened} setOpened={setOpened} setPalettes={setPalettes} paletteIdToDelete={paletteIdToDelete} />
     </PaletteListWrapper>
   )
 }
